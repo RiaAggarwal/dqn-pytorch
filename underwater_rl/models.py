595a26b0538afc92cb81dc98630c5f9dfb352614
Ian Pegg <ianpegg9@gmail.com>
2020-07-19 09:03:59 -0700

    Merge PG

--- underwater_rl/models.py
+++ underwater_rl/models.py
@@ -7,7 +7,10 @@ try:
 except ImportError:
     from torch.utils.model_zoo import load_url as load_state_dict_from_url
 
-__all__ = ['DQNbn', 'DQN', 'DuelingDQN', 'softDQN', 'ResNet', 'resnet18', 'resnet10', 'resnet12', 'resnet14', 'DRQN']
+__all__ = ['DQNbn', 'DQN', 'DuelingDQN', 'softDQN', 'ResNet', 'resnet18', 'resnet10', 'resnet12', 'resnet14',
+           'PolicyGradient', 'DRQN']
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # sets device for model and PyTorch tensors
 
 
 class DQNbn(nn.Module):
@@ -64,6 +67,35 @@ class DQN(nn.Module):
         x = F.relu(self.conv3(x))
         x = F.relu(self.fc4(x.reshape(x.size(0), -1)))
         return self.head(x)
+
+class PolicyGradient(nn.Module):
+    def __init__(self, in_channels=4, n_actions=14):
+        """
+        Initialize Deep Q Network
+
+        Args:
+            in_channels (int): number of input channels
+            n_actions (int): number of outputs
+        """
+        super(PolicyGradient, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
+        # self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        # self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        # self.bn3 = nn.BatchNorm2d(64)
+        self.fc4 = nn.Linear(7 * 7 * 64, 512)
+        self.head = nn.Linear(512, n_actions)
+
+    def forward(self, x):
+        x = x.float() / 255
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.fc4(x.reshape(x.size(0), -1)))
+        action_prob = self.head(x)
+        #action_prob = self.softmax(x)
+        return action_prob
 
 
 class DRQN(DQN):
@@ -142,7 +174,7 @@ class softDQN(nn.Module):
             n_actions (int): number of outputs
         """
         super(softDQN, self).__init__()
-        self.alpha = 100
+        self.alpha = 0.1
         self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
